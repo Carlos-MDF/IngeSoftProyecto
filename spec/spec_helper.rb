54712127394ad61376f620df71d45512c2bd1156
Carlos-MDF <carlosterceros11@gmail.com>
2019-12-13 07:29:38 -0400

    ignore unnecesary test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,6 @@ SimpleCov.start do
   add_filter ".bundle"
   add_filter "template_steps"
   add_filter "lib/errors/invalid_dimension"
+  add_filter "lib/errors/invalid_move"
+  add_filter "lib/errors/invalid_position"
 end
